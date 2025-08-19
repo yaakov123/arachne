0dd256329f6f55427ad40c95e0f3068e9e41267a
ybeckerman <ybeckerman@nexxen.com>
2025-08-20 00:10:08 +0100

    refactor: remove unused types and spec implementation

--- packages/spec/src/index.ts
+++ packages/spec/src/index.ts
@@ -1,15 +1,1 @@
-// Core APISpec domain entrypoint
-// Export types and factories here as the package grows
 
-export interface APISpec {
-  name: string
-  version: string
-  // TODO: add endpoints, models, security schemes, etc.
-}
-
-export function createAPISpec(init: Partial<APISpec> = {}): APISpec {
-  return {
-    name: init.name ?? "",
-    version: init.version ?? "0.1.0",
-  }
-}
