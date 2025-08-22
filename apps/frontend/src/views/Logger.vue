fdb56793703737173635532646665e41799a60ce
ybeckerman <ybeckerman@nexxen.com>
2025-08-22 17:53:33 +0100

    feat: integrate PrimeVue UI components and setup auto-imports

---
+++ apps/frontend/src/views/Logger.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Logger</h1>
+    <Button label="Click" @click="onClick" />
+</template>
+
+<script setup lang="ts">
+function onClick() {
+    console.log('Click')
+}
+</script>
+
+<style scoped></style>
