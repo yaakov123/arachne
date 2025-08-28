225d76a66d1c7026339ec0bd5a463e0c44ac4e57
ybeckerman <ybeckerman@nexxen.com>
2025-08-28 21:21:17 +0100

    refactor: add default ordering to transaction query results by timestamp for improved data retrieval

--- packages/database/src/repositories/transaction.ts
+++ packages/database/src/repositories/transaction.ts
@@ -63,6 +63,9 @@ export class TransactionRepository {
                 projectId,
                 ...options?.where,
             },
+            orderBy: {
+                timestamp: 'desc',
+            },
         })
     }
 
